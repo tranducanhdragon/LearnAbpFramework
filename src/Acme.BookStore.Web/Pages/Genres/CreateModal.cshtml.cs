e8710e933d820a6b9c60274b9b60cce3943f3a4a
DucAnhTranVip <tranducanhvip123@gmail.com>
2021-04-28 11:33:47 +0700

    CreateGenreViewModel

--- src/Acme.BookStore.Web/Pages/Genres/CreateModal.cshtml.cs
+++ src/Acme.BookStore.Web/Pages/Genres/CreateModal.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,10 +8,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Acme.BookStore.Web.Pages.Genres
 {
-    public class CreateModalModel : PageModel
+    public class CreateModalModel : BookStorePageModel
     {
+        public CreateGenreViewModel Genre { get; set; }
         public void OnGet()
         {
+            Genre = new CreateGenreViewModel();
+        }
+
+        public class CreateGenreViewModel
+        {
+            [Required]
+            [StringLength(128)]
+            public string Name { get; set; }
+
         }
     }
 }
